0fadd63002e4d1b6af8e47789d546153a034f3fb
alonsodesansegundo <alonsodesansegundo@gmail.com>
2017-11-06 19:34:30 +0100

    Paso uno

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict"
+/**
+ * @fileOverview Este archivo contiene ejemplos de funciones con for
+ * @author Lucas Alonso
+ * @version 1.0 
+ */
+
+/**
+ * @description sumaArray suma los numeros de una coleccion
+ * @param {Array} numeros Coleccion de numeros que queremos sumar
+ * @return {Number} La suma de los numeros de la coleccion
+ */
+
+ function sumaArray(numeros){
+     let suma=0;
+    
+     return suma;
+ }
